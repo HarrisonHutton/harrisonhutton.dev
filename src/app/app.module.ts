87eaaf03dcb9512e09a29b367481945fc6c45d54
HarrisonHutton <harrison.hutton6@gmail.com>
2022-12-27 19:53:49 -0500

    🎉 Create new AboutMe component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { IntroductionComponent } from './introduction/introduction.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    IntroductionComponent
+    IntroductionComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
