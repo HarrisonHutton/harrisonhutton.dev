b0a7e1a7b64d5990d1e76b5aba6d3412fa03ba2b
HarrisonHutton <harrison.hutton6@gmail.com>
2022-12-27 19:36:09 -0500

    🎉 Create new Introduction component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { IntroductionComponent } from './introduction/introduction.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    IntroductionComponent
   ],
   imports: [
     BrowserModule,
