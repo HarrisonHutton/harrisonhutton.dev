6d5404c2148eb2395ce11a85e3fe72e96efa7314
HarrisonHutton <harrison.hutton6@gmail.com>
2022-12-30 15:14:25 -0500

    💄 Implement disappearing header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, Input } from '@angular/core';
+import { Component, HostListener, Input, Renderer2 } from '@angular/core';
 import { Section } from '../@interfaces/section';
 
 @Component({
@@ -9,5 +9,26 @@ import { Section } from '../@interfaces/section';
 export class HeaderComponent {
 
   @Input() sections: Section[] = [];
+
+  private previousScrollY = 0;
+
+  constructor(private renderer: Renderer2) {}
+
+  /* Make header disappear on scroll down and reappear on scroll up. */
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(event: UIEvent) {
+    const currentScrollY = window.scrollY;
+    const header = this.renderer.selectRootElement('header', true);
+
+    if (currentScrollY > this.previousScrollY) {
+      this.renderer.addClass(header, 'header--hidden', );
+    }
+    else {
+      this.previousScrollY = 0;
+      this.renderer.removeClass(header, 'header--hidden');
+    }
+
+    this.previousScrollY = currentScrollY;
+  }
 
 }
