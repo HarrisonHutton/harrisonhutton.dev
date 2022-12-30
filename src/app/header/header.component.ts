e0f457ef4e90c64012847cbb1da19a0538e6e054
HarrisonHutton <harrison.hutton6@gmail.com>
2022-12-30 13:56:22 -0500

    ✨ Redesign navbar to be sticky, glassy flexbox

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,23 +8,6 @@ import { Section } from '../@interfaces/section';
 })
 export class HeaderComponent {
 
-  appRoot = document.getElementsByTagName('app-root')[0];
-
   @Input() sections: Section[] = [];
-
-  @HostListener('click', ['$event.target'])
-  onClick(target: HTMLElement) {
-    if (target.classList.contains('nav__link')) {
-      this.removeNav();
-    }
-  }
-
-  toggleNav() {
-    this.appRoot.classList.toggle('nav-open');
-  }
-
-  removeNav() {
-    this.appRoot.classList.remove('nav-open');
-  }
 
 }
