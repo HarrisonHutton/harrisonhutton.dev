8f551c95eab80ff972f732aca0b864e2899df9d1
HarrisonHutton <harrison.hutton6@gmail.com>
2022-12-30 16:35:49 -0500

    🐛 Fix header disappearing on mobile scroll

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -18,13 +18,17 @@ export class HeaderComponent {
   @HostListener('window:scroll', ['$event'])
   onWindowScroll(event: UIEvent) {
     const currentScrollY = window.scrollY;
+
+    /* Prevent the header from disappearing on mobile devices when the user
+     * scrolls to the top of the page. */
+    if (currentScrollY < 0) return;
+
     const header = this.renderer.selectRootElement('header', true);
 
     if (currentScrollY > this.previousScrollY) {
-      this.renderer.addClass(header, 'header--hidden', );
+      this.renderer.addClass(header, 'header--hidden');
     }
     else {
-      this.previousScrollY = 0;
       this.renderer.removeClass(header, 'header--hidden');
     }
 
