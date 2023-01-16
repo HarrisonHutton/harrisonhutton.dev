f05bcc242f43314e1e1a82bec9da16abf293265c
HarrisonHutton <harrison.hutton6@gmail.com>
2023-01-16 12:57:50 -0500

    🎉 Generate new audio player component

---
+++ src/app/audio-player/audio-player.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-audio-player',
+  templateUrl: './audio-player.component.html',
+  styleUrls: ['./audio-player.component.scss']
+})
+export class AudioPlayerComponent {
+
+}
