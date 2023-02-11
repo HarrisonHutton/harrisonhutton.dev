1cc8cc90988442f115f5a65f32f1ee3a902843ac
HarrisonHutton <harrison.hutton6@gmail.com>
2023-02-11 17:18:23 -0500

    ✨ Add fade in animations to my projects

--- src/app/my-work/my-work.component.ts
+++ src/app/my-work/my-work.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-my-work',
   templateUrl: './my-work.component.html',
   styleUrls: ['./my-work.component.scss']
 })
-export class MyWorkComponent {
+export class MyWorkComponent implements AfterViewInit {
+
+  /* Animation code */
+  observer: IntersectionObserver = new IntersectionObserver((entries) => { 
+    entries.forEach(entry => {
+      if (entry.isIntersecting || entry.intersectionRatio > 0) {
+        // Only run the animation once, so do not remove the 'visible' class
+        entry.target.classList.add('visible');
+      }
+      else {
+        // If the element is not visible, remove the 'visible' class
+        entry.target.classList.remove('visible');
+      }
+    });
+  });
+
+  ngAfterViewInit() {
+    // Animation code
+    const hiddenElements = document.querySelectorAll('.hidden');
+    hiddenElements.forEach(element => this.observer.observe(element));
+  }
 
 }
